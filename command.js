7f33689aa78046ac451b7f6119fd0f8c5bc11945
pcdelatara <pcdelatara@gmail.com>
2025-05-01 21:17:14 +0300

    Update command.js

--- command.js
+++ command.js
@@ -1,8 +1,3 @@
-bsline1.innerHTML = "Your pc in now mine, don't try to get rid of me, you will not be able to.";
-bsline2.innerHTML = "Your pc is now dead, try enjoing it.";
-bsline4.innerHTML = "NO MORE INFORMATION FOR YOU : ";
-bsline5.innerHTML = "666://nopc.death";
-bsline6.innerHTML = ""If you call a support person, give them this info:;
-bsline7.innerHTML = "I_HAVE_BEEN_PRANKED";
+
 bsod.style.background = "#FF0000";
 bsodsad.innerHTML = ":}";
